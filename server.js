867300fa08592d89c0c48162a6727a83a82a8652
Yaroslav <yomelianenkowork@gmail.com>
2023-10-08 19:55:00 +0300

    fixes

--- server.js
+++ server.js
@@ -19,16 +19,16 @@ app.use("/api/articles", require("./routes/articleRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
 
 //Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.use(errorHandler);
 
